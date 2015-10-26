6db4cb6820dfe484e9badc7145e91113207519b7
Telmo Sampaio <telmos@microsoft.com>
2015-10-26 11:13:17 -0700

    Fixed quotes

--- IaaSStory/IaaSStory/Templates/04-Custom extension/iisaspnet.ps1
+++ IaaSStory/IaaSStory/Templates/04-Custom extension/iisaspnet.ps1
@@ -5,15 +5,15 @@ Configuration IISASPNET
     #Install the IIS Role
     WindowsFeature IIS
     {
-      Ensure = “Present”
-      Name = “Web-Server”
+      Ensure = "Present"
+      Name = "Web-Server"
     }
 
     #Install ASP.NET 4.5
     WindowsFeature ASP
     {
-      Ensure = “Present”
-      Name = “Web-Asp-Net45”
+      Ensure = "Present"
+      Name = "Web-Asp-Net45"
     }
 
     WindowsFeature WebServerManagementConsole
